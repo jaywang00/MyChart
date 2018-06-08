5f620200b2adc1368ef67e3bc7abc1820a5278bb
Jay Wang <ielcw00@gmail.com>
2018-06-08 11:31:13 +0800

    v1.00_LineChart_MoreChanged_b4_2_lines

--- app/src/main/java/com/example/demouicontrol/mychart/MainActivity.java
+++ app/src/main/java/com/example/demouicontrol/mychart/MainActivity.java
@@ -17,36 +17,21 @@ import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
-    //##### Add here
+
     LineChart chart ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //##### Add here
         chart = (LineChart) findViewById(R.id.chart);
         init();
     }
 
-    //##### Add here
     void init(){
-//        YourData[] dataObjects = ...;
 
         List<Entry> entries = new ArrayList<Entry>();
 
-//        for (YourData data : dataObjects) {
-//
-//            // turn your data into Entry objects
-//            entries.add(new Entry(data.getValueX(), data.getValueY()));
-//        }
-//        entries.add(new Entry(1,30));
-//        entries.add(new Entry(3,45));
-//        entries.add(new Entry(5,40));
-//        entries.add(new Entry(7,20));
-//        entries.add(new Entry(9,70));
-//        entries.add(new Entry(11,40));
-//### Change here
         Random r = new Random();
         for(int i=1; i<=365; i++){
             entries.add(new Entry(i,r.nextInt(500)));
@@ -54,18 +39,17 @@ public class MainActivity extends AppCompatActivity {
 
         LineDataSet dataSet = new LineDataSet(entries, "每日花費"); // add entries to dataset
         dataSet.setColor(Color.BLUE);
-        //### Add here
+
         dataSet.setValueTextSize(10f);
         dataSet.setValueTextColor(Color.RED); // styling, ...
 
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
 
-//### Add here
         XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setTextSize(15);
-        xAxis.setTextColor(Color.RED);
+        xAxis.setTextColor(Color.BLACK);
 
         YAxis leftAxis = chart.getAxisLeft();
         leftAxis.setTextSize(15);
@@ -75,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
         rightAxis.setEnabled(false);
         chart.setVisibleXRangeMaximum(30);
 
-        //        xAxis.setLabelCount(5);
         chart.invalidate(); // refresh
 
     }
