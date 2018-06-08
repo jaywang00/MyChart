6857cfcbd5a93cdd7f5178660db3aa4d21c2d5b1
Jay Wang <ielcw00@gmail.com>
2018-06-08 11:03:18 +0800

    v1.00_LineChart_MoreChanged

--- app/src/main/java/com/example/demouicontrol/mychart/MainActivity.java
+++ app/src/main/java/com/example/demouicontrol/mychart/MainActivity.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -37,19 +40,43 @@ public class MainActivity extends AppCompatActivity {
 //            // turn your data into Entry objects
 //            entries.add(new Entry(data.getValueX(), data.getValueY()));
 //        }
-        entries.add(new Entry(1,30));
-        entries.add(new Entry(3,45));
-        entries.add(new Entry(5,40));
-        entries.add(new Entry(7,20));
-        entries.add(new Entry(9,70));
-        entries.add(new Entry(11,40));
+//        entries.add(new Entry(1,30));
+//        entries.add(new Entry(3,45));
+//        entries.add(new Entry(5,40));
+//        entries.add(new Entry(7,20));
+//        entries.add(new Entry(9,70));
+//        entries.add(new Entry(11,40));
+//### Change here
+        Random r = new Random();
+        for(int i=1; i<=365; i++){
+            entries.add(new Entry(i,r.nextInt(500)));
+        }
 
-        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        LineDataSet dataSet = new LineDataSet(entries, "每日花費"); // add entries to dataset
         dataSet.setColor(Color.BLUE);
+        //### Add here
+        dataSet.setValueTextSize(10f);
         dataSet.setValueTextColor(Color.RED); // styling, ...
 
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
+
+//### Add here
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(15);
+        xAxis.setTextColor(Color.RED);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setTextSize(15);
+        leftAxis.setTextColor(Color.RED);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+        chart.setVisibleXRangeMaximum(30);
+
+        //        xAxis.setLabelCount(5);
         chart.invalidate(); // refresh
+
     }
 }
